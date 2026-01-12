d5d4404d0ad8f903dd29d1c1d1f327f3d78a47b8
timstasevich <tim.stasevich@colostate.edu>
2026-01-12 13:39:18 -0700

    Update crop_ops init and add translation stalling quantification notebook

--- croparray/crop_ops/__init__.py
+++ croparray/crop_ops/__init__.py
@@ -1,3 +1,9 @@
-from .measure import spot_detect_and_qc
+from .measure import spot_detect_and_qc, binarize_crop_manual, binarize_crop
+from .apply import  apply_crop_op  # whatever is in apply.py
 
-__all__ = ["spot_detect_and_qc"]
+__all__ = [
+    "spot_detect_and_qc",
+    "binarize_crop",
+    "binarize_crop_manual",
+    "apply_crop_op",
+]
